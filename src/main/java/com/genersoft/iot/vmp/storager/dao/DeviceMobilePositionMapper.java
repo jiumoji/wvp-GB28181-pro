c165d93bce2825f489af609dbb0ae2934e1de6e4
dapeng <845999248@qq.com>
2022-06-02 14:10:52 +0800

    修改DeviceMobilePositionMapper

--- src/main/java/com/genersoft/iot/vmp/storager/dao/DeviceMobilePositionMapper.java
+++ src/main/java/com/genersoft/iot/vmp/storager/dao/DeviceMobilePositionMapper.java
@@ -10,24 +10,24 @@ import org.apache.ibatis.annotations.*;
 //@Repository
 public interface DeviceMobilePositionMapper {
 
-    @Insert("INSERT INTO device_mobile_position (deviceId,channelId, deviceName, time, longitude, latitude, altitude, speed, direction, reportSource, geodeticSystem, cnLng, cnLat) " +
+    @Insert("INSERT INTO device_mobile_position (device_id,channel_id, device_name, time, longitude, latitude, altitude, speed, direction, report_source, geodetic_system, cnLng, cnLat) " +
             "VALUES ('${deviceId}','${channelId}', '${deviceName}', '${time}', ${longitude}, ${latitude}, ${altitude}, ${speed}, ${direction}, '${reportSource}', '${geodeticSystem}', '${cnLng}', '${cnLat}')")
     int insertNewPosition(MobilePosition mobilePosition);
 
     @Select(value = {" <script>" +
     "SELECT * FROM device_mobile_position" +
-    " WHERE deviceId = #{deviceId} and channelId = #{channelId} " +
+    " WHERE device_id = #{deviceId} and channel_id = #{channelId} " +
     "<if test=\"startTime != null\"> AND time&gt;=#{startTime}</if>" +
     "<if test=\"endTime != null\"> AND time&lt;=#{endTime}</if>" +
     " ORDER BY time ASC" +
     " </script>"})
     List<MobilePosition> queryPositionByDeviceIdAndTime(String deviceId, String channelId, String startTime, String endTime);
 
-    @Select("SELECT * FROM device_mobile_position WHERE deviceId = #{deviceId}" +
+    @Select("SELECT * FROM device_mobile_position WHERE device_id = #{deviceId}" +
             " ORDER BY time DESC LIMIT 1")
     MobilePosition queryLatestPositionByDevice(String deviceId);
 
-    @Delete("DELETE FROM device_mobile_position WHERE deviceId = #{deviceId}")
+    @Delete("DELETE FROM device_mobile_position WHERE device_id = #{deviceId}")
     int clearMobilePositionsByDeviceId(String deviceId);
 
 }
